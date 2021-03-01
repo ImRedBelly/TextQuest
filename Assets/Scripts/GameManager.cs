76f43318c087b54929be4155cafc82a535926b4a
Dmitry <ImRedBelly@gmail.com>
2021-03-01 18:10:11 +0300

    finally

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text choiceContent;
+    public Text textContent;
+    public Image backgroundImage;
+    public Step startStep;
+    public Step currentStep;
+
+    void Start()
+    {
+        currentStep = startStep;
+        UpdateText();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChoiseAnswer(0);
+            UpdateText();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChoiseAnswer(1);
+            UpdateText();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChoiseAnswer(2);
+            UpdateText();
+        }
+    }
+    void ChoiseAnswer(int index)
+    {
+        currentStep = currentStep.nextSteps[index];
+    }
+    void UpdateText()
+    {
+        choiceContent.text = currentStep.choiceContent;
+        textContent.text = currentStep.textContent;
+        backgroundImage.sprite = currentStep.backgroundSprite;
+    }
+
+}
---
+++ Assets/Scripts/Step.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Step : MonoBehaviour
+{
+    [TextArea(15, 50)]
+    public string choiceContent;
+
+    [TextArea(15, 50)]
+    public string textContent;
+
+    public Sprite backgroundSprite;
+    public Step[] nextSteps;
+}
